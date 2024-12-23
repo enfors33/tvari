32c1d8a3b56519a7ccfc7d3e452a3dc179ae4050
enfors33 <gromikovlad.2008@gmail.com>
2024-12-24 01:17:06 +0200

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,110 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'наушники Logitech G435 LightSpeed Wireless Headset White',
+        price: '59.99 €',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/item-1.jpg',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 1,
+        title: 'оперативная память Kingston Fury Beast 32GB DDR5 5200MHz DIMM KF552C40BBK2-32',
+        price: '101.10 €',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/item-2.jpg',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 2,
+        title: 'игровой контроллер Microsoft Xbox One / Xbox One S / Xbox One X / PC Astral Purple',
+        price: '62.90 €',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/item-3.jpg',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 3,
+        title: 'Пидрартем',
+        price: '1 нагетс',
+        address: 'Казань, р-н Вахитовский',
+        date: '666',
+        img: '/image/artem.png',
+        desc: 'заебал'
+    },
+    {
+        id: 4,
+        title: 'Бебка',
+        price: '3 белых брата , артем',
+        address: 'аквапарк',
+        date: '666',
+        img: '/image/bebo.png',
+        desc: 'Негр'
+    },
+    {
+        id: 5,
+        title: 'свинюка, ',
+        price: '1 душа бебыча ',
+        address: 'свиноферма',
+        date: '666',
+        img: '/image/karimka.jpg',
+        desc: 'отличная колбаса, отменный продукт, нагетсы '
+    },
+    {
+        id: 6,
+        title: 'игровой контроллер Microsoft Xbox One / Xbox One S / Xbox One X / PC Astral Purple',
+        price: '62.90 €',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/item-3.jpg',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 7,
+        title: 'Четвертый товар',
+        price: '173 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-image.png',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 8,
+        title: 'Пятый товар',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-image.png',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 9,
+        title: 'Шестой товар',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-image.png',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 10,
+        title: 'Пятый товар',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-image.png',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+    {
+        id: 11,
+        title: 'Шестой товар',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-image.png',
+        desc: 'Продаю не спеша самокат в хорошем состоянии.'
+    },
+];
